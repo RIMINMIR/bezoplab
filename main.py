803223cc9a547387ce638449bc9144a648f48d02
RIMINMIR <11111rr8@gmail.com>
2021-12-17 15:44:50 +0300

    Добавлен класс ну какой то там

--- main.py
+++ main.py
@@ -3,6 +3,13 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+class SomeClass:
+    def __init__(self):
+        self.text = 'Член'
+
+    def get_text(self):
+        return self.text
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
